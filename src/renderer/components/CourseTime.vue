19a1dc1e882f447419a646e6c83400176ca58fe2
Lemon <3090957190@qq.com>
2024-12-17 20:45:56 +0800

    课程开始和结束时间组件

---
+++ src/renderer/components/CourseTime.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { defineOptions } from 'vue';
+
+defineOptions({
+     name: 'CourseTime'
+});
+
+const props = defineProps({
+    startTime: {
+        type: String,
+        required: true,
+        default: '08:00'
+    },
+    endTime: {
+        type: String,
+        required: true,
+        default: '08:45'
+    }
+});
+</script>
+
+<template>
+    <div class="course-time">
+        <div class="start">{{ startTime }}</div>
+        <div class="end">{{ endTime }}</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.course-time {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    color: black;
+    text-align: center;
+    font-size: 12px;
+
+    .start, .end {
+        width: 100%;
+        margin: 5px 0;
+    }
+}
+</style>
